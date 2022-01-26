936bd98fb280e558c65ddcaf8e2b269efe677d84
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-01-26 22:32:46 +0100

    Clean up main
    - move prints out of printArgs
    - fix obsolete doc

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -19,10 +19,10 @@ const (
 
 var (
 	url         string
-	concurrency int           // Number of connections to run concurrently.
+	concurrency int           // Number of connections to run concurrently
 	requests    int           // Number of requests to run, use duration as exit condition if omitted.
-	duration    time.Duration // Duration of test, in seconds.
-	timeout     time.Duration // Timeout for each http request, in seconds.
+	duration    time.Duration // Duration of test
+	timeout     time.Duration // Timeout for each http request
 )
 
 func parseArgs() {
@@ -30,10 +30,24 @@ func parseArgs() {
 
 	flag.IntVar(&concurrency, "c", DefaultConcurrency, "Number of connections to run concurrently")
 	flag.IntVar(&requests, "r", DefaultRequests, "Number of requests to run, use duration as exit condition if omitted")
-	flag.DurationVar(&duration, "d", DefaultDuration, "Duration of test, in seconds")
-	flag.DurationVar(&timeout, "t", DefaultTimeout, "Timeout for each http request, in seconds")
+	flag.DurationVar(&duration, "d", DefaultDuration, "Duration of test")
+	flag.DurationVar(&timeout, "t", DefaultTimeout, "Timeout for each http request")
 	flag.Parse()
+}
 
+func main() {
+	parseArgs()
+	printConfig()
+
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+
+	rec := request.Do(ctx, requests, concurrency, url, timeout)
+
+	fmt.Println("total:", len(rec))
+}
+
+func printConfig() {
 	fmt.Printf("Testing url: %s\n", url)
 	fmt.Printf("concurrency: %d\n", concurrency)
 	if requests > 0 {
@@ -42,15 +56,4 @@ func parseArgs() {
 	fmt.Printf("duration: %s\n", duration)
 
 	println()
-}
-
-func main() {
-	parseArgs()
-
-	ctx, cancel := context.WithTimeout(context.Background(), duration)
-	defer cancel()
-
-	rec := request.Do(ctx, requests, concurrency, url, timeout)
-
-	fmt.Println("total:", len(rec))
 }
