6ed677d3544f76ae184ae35035e76801ac2aa7aa
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-01-27 23:27:33 +0100

    Refactor configparser unit tests
    - implement and use setTempValue
    - add some documentation

--- configparser/parse_test.go
+++ configparser/parse_test.go
@@ -11,7 +11,10 @@ import (
 	"github.com/benchttp/runner/configparser"
 )
 
-const testdataConfigPath = "../test/testdata/config"
+const (
+	testdataConfigPath = "../test/testdata/config"
+	testURL            = "http://localhost:9999?fib=30&delay=200ms"
+)
 
 var supportedExt = []string{
 	".yml",
@@ -28,12 +31,14 @@ func TestParse(t *testing.T) {
 
 			gotCfg, err := configparser.Parse(fname)
 			if err != nil {
-				// critical error, do not continue the test suite
+				// critical error, stop the test
 				t.Fatal(err)
 			}
 
+			gotURL, expURL := gotCfg.Request.URL, expCfg.Request.URL
+
 			// compare *url.URLs separately, as they contain unpredictable values
-			// they need special treatment
+			// they need special checks
 			if !sameURL(gotCfg.Request.URL, expCfg.Request.URL) {
 				t.Errorf(
 					"unexpected parsed URL: exp %v, got %v",
@@ -42,35 +47,32 @@ func TestParse(t *testing.T) {
 			}
 
 			// replace unpredictable values (undetermined query params order)
-			gotCfg.Request.URL.RawQuery = "replaced by test"
-			expCfg.Request.URL.RawQuery = "replaced by test"
+			restoreGotCfg := setTempValue(&gotURL.RawQuery, "<replaced by test>")
+			restoreExpCfg := setTempValue(&expURL.RawQuery, "<replaced by test>")
 
 			if !reflect.DeepEqual(gotCfg, expCfg) {
 				t.Errorf("unexpected parsed config: exp %s\ngot %s", expCfg, gotCfg)
 			}
+
+			restoreExpCfg()
+			restoreGotCfg()
 		}
 	})
 }
 
+// helpers
+
 // newExpConfig returns the expected config.Config result after parsing
 // one of the config files in testdataConfigPath.
 func newExpConfig() config.Config {
-	u, _ := url.Parse("http://localhost:9999?fib=30&delay=200ms")
+	u, _ := url.Parse(testURL)
 	return config.Config{
-		Request: struct {
-			Method string
-			URL    *url.URL
-		}{
+		Request: config.Request{
 			Method: "GET",
 			URL:    u,
 		},
 
-		RunnerOptions: struct {
-			Requests       int
-			Concurrency    int
-			GlobalTimeout  time.Duration
-			RequestTimeout time.Duration
-		}{
+		RunnerOptions: config.RunnerOptions{
 			Requests:       100,
 			Concurrency:    1,
 			GlobalTimeout:  60 * time.Second,
@@ -79,18 +81,29 @@ func newExpConfig() config.Config {
 	}
 }
 
+// sameURL returns true if a and b are the same *url.URL, taking into account
+// the undeterministic nature of their RawQuery.
 func sameURL(a, b *url.URL) bool {
+	// check query params equality via Query() rather than RawQuery
 	if !reflect.DeepEqual(a.Query(), b.Query()) {
 		return false
 	}
 
-	rqa, rqb := a.RawQuery, b.RawQuery
-	defer func() {
-		a.RawQuery = rqa
-		b.RawQuery = rqb
-	}()
-	a.RawQuery = ""
-	b.RawQuery = ""
+	// temporarily set RawQuery to a determined value
+	for _, u := range []*url.URL{a, b} {
+		defer setTempValue(&u.RawQuery, "<replaced by test>")()
+	}
 
+	// we can now rely on deep equality check
 	return reflect.DeepEqual(a, b)
+}
+
+// setTempValue sets *ptr to val and returns a restore func that sets *ptr
+// back to its previous value.
+func setTempValue(ptr *string, val string) (restore func()) {
+	previousValue := *ptr
+	*ptr = val
+	return func() {
+		*ptr = previousValue
+	}
 }
