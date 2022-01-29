338711b9e95ae52885bf799b113bf4c7b8107075
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-01-29 17:11:43 +0100

    Write unit tests for config.New
    - use default config as base
    - override default with params

--- config/config_test.go
+++ config/config_test.go
@@ -15,7 +15,7 @@ func TestMerge(t *testing.T) {
 		cfgZero := config.Config{}
 
 		if got := config.Merge(cfgBase, cfgZero); !reflect.DeepEqual(got, cfgBase) {
-			t.Errorf("overrode with zero values: exp %#v\ngot %#v", cfgBase, got)
+			t.Errorf("overrode with zero values:\nexp %#v\ngot %#v", cfgBase, got)
 		}
 	})
 
@@ -38,7 +38,7 @@ func TestMerge(t *testing.T) {
 
 		if got := config.Merge(cfgBase, cfgOver); !reflect.DeepEqual(got, cfgOver) {
 			t.Errorf(
-				"did not override with non-zero values: exp %#v\ngot %#v",
+				"did not override with non-zero values\nexp %#v\ngot %#v",
 				cfgOver, got,
 			)
 		}
@@ -65,12 +65,53 @@ func TestMerge(t *testing.T) {
 
 		if got := config.Merge(cfgBase, cfgOver); got != exp {
 			t.Errorf(
-				"did not selectively override with non-zero values: exp %#v\ngot %#v",
+				"did not selectively override with non-zero values\nexp %#v\ngot %#v",
 				exp, got,
 			)
 		}
 	})
 }
+
+func TestNew(t *testing.T) {
+	t.Run("use default config as base", func(t *testing.T) {
+		exp := config.Default()
+		if got := config.New("", 0, 0, 0, 0); !reflect.DeepEqual(got, exp) {
+			t.Errorf("did not use default config as base:\nexp %#v\ngot %#v", exp, got)
+		}
+	})
+
+	t.Run("override default with params", func(t *testing.T) {
+		var (
+			rawURL      = "http://example.com"
+			urlURL, _   = url.Parse(rawURL)
+			requests    = 10
+			concurrency = 10
+			reqTimeout  = 2 * time.Second
+			glbTimeout  = 10 * time.Second
+		)
+
+		exp := config.Config{
+			Request: config.Request{
+				Method:  "GET",
+				URL:     urlURL,
+				Timeout: reqTimeout,
+			},
+			RunnerOptions: config.RunnerOptions{
+				Requests:      requests,
+				Concurrency:   concurrency,
+				GlobalTimeout: glbTimeout,
+			},
+		}
+
+		got := config.New(rawURL, requests, concurrency, reqTimeout, glbTimeout)
+
+		if !reflect.DeepEqual(got, exp) {
+			t.Errorf("did not override with params:\nexp %#v\ngot %#v", exp, got)
+		}
+	})
+}
+
+// helpers
 
 func newConfig() config.Config {
 	return config.Config{
