e7a8c70ae9278a34c4f5105b2ccd730729431036
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-02-01 17:57:52 +0100

    Implement error handling for Dispatcher.Do

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -2,26 +2,31 @@ package dispatcher
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
 
 	"golang.org/x/sync/semaphore"
 )
 
+var ErrInvalidValue = errors.New("invalid value")
+
 type Dispatcher interface {
-	Do(ctx context.Context, maxIter int, callback func())
+	Do(ctx context.Context, maxIter int, callback func()) error
 }
 
 type dispatcher struct {
-	sem *semaphore.Weighted
+	numWorker int
+	sem       *semaphore.Weighted
 }
 
 // Do concurrently executes callback at most maxIter times or until ctx is done
 // or canceled. Concurrency is handled leveraging the semaphore pattern, which
 // ensures at most Dispatcher.numWorkers goroutines are spawned at the same time.
-func (d dispatcher) Do(ctx context.Context, maxIter int, callback func()) {
-	maxIter = sanitizeMaxIter(maxIter)
-	callback = sanitizeCallback(callback)
+func (d dispatcher) Do(ctx context.Context, maxIter int, callback func()) error {
+	if err := d.validate(maxIter, callback); err != nil {
+		return err
+	}
 
 	wg := sync.WaitGroup{}
 
@@ -45,6 +50,23 @@ func (d dispatcher) Do(ctx context.Context, maxIter int, callback func()) {
 	}
 
 	wg.Wait()
+	return nil
+}
+
+func (d dispatcher) validate(maxIter int, callback func()) error {
+	if maxIter < 1 {
+		return fmt.Errorf("%w: maxIter: must be < 1, got %d", ErrInvalidValue, maxIter)
+	}
+	if maxIter < d.numWorker {
+		return fmt.Errorf(
+			"%w: maxIter: must be >= numWorker, got numWorker == %d, maxIter == %d",
+			ErrInvalidValue, maxIter, d.numWorker,
+		)
+	}
+	if callback == nil {
+		return fmt.Errorf("%w: callback: must be non-nil", ErrInvalidValue)
+	}
+	return nil
 }
 
 // New returns a Dispatcher initialized with numWorker.
@@ -53,19 +75,5 @@ func New(numWorker int) Dispatcher {
 		panic(fmt.Sprintf("invalid numWorker value: must be > 1, got %d", numWorker))
 	}
 	sem := semaphore.NewWeighted(int64(numWorker))
-	return dispatcher{sem: sem}
-}
-
-func sanitizeMaxIter(maxIter int) int {
-	if maxIter < 0 {
-		return 0
-	}
-	return maxIter
-}
-
-func sanitizeCallback(callback func()) func() {
-	if callback == nil {
-		return func() {}
-	}
-	return callback
+	return dispatcher{sem: sem, numWorker: numWorker}
 }
