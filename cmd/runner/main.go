c87e05b2f63b5b845eae33ea89c99c2bb48a0d23
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-02-16 22:46:47 +0100

    Use flag.Value to set the request body via CLI
    - config.ParseBody -> main.bodyValue.Set

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -28,7 +28,7 @@ var (
 	// If type is "raw", content is the data as a string. If type is "file",
 	// content is the path to the file holding the data. Note: only "raw"
 	// is supported at the moment.
-	body           string
+	body           config.Body
 	concurrency    int           // Number of connections to run concurrently
 	requests       int           // Number of requests to run, use duration as exit condition if omitted.
 	interval       time.Duration // Minimum duration between two groups of requests
@@ -50,6 +50,8 @@ func parseArgs() {
 	flag.StringVar(&uri, config.FieldURL, "", "Target URL to request")
 	// request header
 	flag.Var(headerValue{header: &header}, config.FieldHeader, "HTTP request header")
+	// request body
+	flag.Var(bodyValue{body: &body}, config.FieldBody, "HTTP request body")
 	// concurrency
 	flag.IntVar(&concurrency, config.FieldConcurrency, 0, "Number of connections to run concurrently")
 	// requests number
@@ -62,8 +64,6 @@ func parseArgs() {
 	flag.DurationVar(&globalTimeout, config.FieldGlobalTimeout, 0, "Max duration of test")
 	// request method
 	flag.StringVar(&method, config.FieldMethod, "", "HTTP request method")
-	// body
-	flag.StringVar(&body, config.FieldBody, "", "HTTP request body")
 	flag.Parse()
 }
 
@@ -108,11 +108,6 @@ func parseConfig() (cfg config.Global, err error) {
 		return
 	}
 
-	body, err := config.ParseBody(body)
-	if err != nil {
-		return cfg, nil
-	}
-
 	cliCfg := config.Global{
 		Request: config.Request{
 			Header: header,
@@ -155,12 +150,60 @@ func (v headerValue) String() string {
 
 // Set reads input string in format "key:value" and appends value
 // to the key's values of the referenced header.
-func (v headerValue) Set(in string) error {
-	keyval := strings.Split(in, ":")
+func (v headerValue) Set(raw string) error {
+	keyval := strings.SplitN(raw, ":", 2)
 	if len(keyval) != 2 {
-		return errors.New(`expect format key:value`)
+		return errors.New(`expect format "<key>:<value>"`)
 	}
 	key, val := keyval[0], keyval[1]
 	(*v.header)[key] = append((*v.header)[key], val)
+	return nil
+}
+
+// bodyValue implements flag.Value
+type bodyValue struct {
+	body *config.Body
+}
+
+// String returns a string representation of the referenced body.
+func (v bodyValue) String() string {
+	return fmt.Sprint(v.body)
+}
+
+// Set reads input string in format "type:content" and sets
+// the referenced body accordingly.
+//
+// If type is "raw", content is the data as a string.
+//	"raw:{\"key\":\"value\"}" // escaped JSON
+//	"raw:text" // plain text
+// If type is "file", content is the path to the file holding the data.
+//	"file:./path/to/file.txt"
+//
+// Note: only type "raw" is supported at the moment.
+func (v bodyValue) Set(raw string) error {
+	errFormat := fmt.Errorf(`expect format "<type>:<content>", got "%s"`, raw)
+
+	if raw == "" {
+		return errFormat
+	}
+
+	split := strings.SplitN(raw, ":", 2)
+	if len(split) != 2 {
+		return errFormat
+	}
+
+	btype, bcontent := split[0], split[1]
+	if bcontent == "" {
+		return errFormat
+	}
+
+	switch btype {
+	case "raw":
+		*v.body = config.NewBody(btype, bcontent)
+	// case "file":
+	// 	// TODO
+	default:
+		return fmt.Errorf(`unsupported type: %s (only "raw" accepted`, btype)
+	}
 	return nil
 }
