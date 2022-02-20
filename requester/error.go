16277b03cf32323f4ced2cd52ce6db7dd7a8a21d
moreirathomas <moreirathomas97@gmail.com>
2022-02-20 02:10:57 +0100

    refactor: change Record.Error from error to string
    - use recordErr to convert and mark a raw err to a formatted string

--- requester/error.go
+++ requester/error.go
@@ -1,6 +1,9 @@
 package requester
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	// ErrConnection is returned when the Requester fails to connect to
@@ -11,3 +14,9 @@ var (
 	// ErrCanceled is returned when the Requester.Run context is canceled.
 	ErrCanceled = errors.New("canceled")
 )
+
+// recordErr wraps and returns err as a string, marking it as an error
+// that happened when recording the request.
+func recordErr(err error) string {
+	return fmt.Sprintf("recording error: %s", err)
+}
