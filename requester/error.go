61f6df610f137e14e5cbdb8ed6a2afdb00c387c7
moreirathomas <moreirathomas97@gmail.com>
2022-02-20 02:11:20 +0100

    feat: remove unused ErrReporting

--- requester/error.go
+++ requester/error.go
@@ -9,8 +9,6 @@ var (
 	// ErrConnection is returned when the Requester fails to connect to
 	// the requested URL.
 	ErrConnection = errors.New("connection error")
-	// ErrReporting is returned when the Requester fails to send the report.
-	ErrReporting = errors.New("reporting error")
 	// ErrCanceled is returned when the Requester.Run context is canceled.
 	ErrCanceled = errors.New("canceled")
 )
