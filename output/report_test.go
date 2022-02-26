2b70c590ec4321cd311d475956a6bbeaf1a51e09
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-02-26 01:23:26 +0100

    Write unit tests for Report.HTTPRequest

--- output/report_test.go
+++ output/report_test.go
@@ -1,7 +1,13 @@
 package output_test
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
@@ -16,7 +22,7 @@ func TestReport_String(t *testing.T) {
 	t.Run("return default summary if template is empty", func(t *testing.T) {
 		const tpl = ""
 
-		rep := output.New(newBenchmark(), newConfig(tpl))
+		rep := output.New(newBenchmark(), newConfigWithTemplate(tpl))
 		checkSummary(t, rep.String())
 	})
 
@@ -24,7 +30,7 @@ func TestReport_String(t *testing.T) {
 		const tpl = "{{ .Benchmark.Length }}"
 
 		bk := newBenchmark()
-		rep := output.New(bk, newConfig(tpl))
+		rep := output.New(bk, newConfigWithTemplate(tpl))
 
 		if got, exp := rep.String(), strconv.Itoa(bk.Length); got != exp {
 			t.Errorf("\nunexpected output\nexp %s\ngot %s", exp, got)
@@ -34,7 +40,7 @@ func TestReport_String(t *testing.T) {
 	t.Run("fallback to default summary if template is invalid", func(t *testing.T) {
 		const tpl = "{{ .Marcel.Patulacci }}"
 
-		rep := output.New(newBenchmark(), newConfig(tpl))
+		rep := output.New(newBenchmark(), newConfigWithTemplate(tpl))
 		got := rep.String()
 		split := strings.Split(got, "Falling back to default summary:\n")
 
@@ -51,6 +57,19 @@ func TestReport_String(t *testing.T) {
 	})
 }
 
+func TestReport_HTTPRequest(t *testing.T) {
+	t.Run("generate gob-encoded POST request to target endpoint", func(t *testing.T) {
+		bk, cfg := newBenchmark(), newConfigWithTemplate("")
+		rep := output.New(bk, cfg)
+
+		req, err := rep.HTTPRequest()
+		if err != nil {
+			t.Fatalf("unexpected error:\n%s", err)
+		}
+		checkRequest(t, req, rep)
+	})
+}
+
 // helpers
 
 func newBenchmark() requester.Benchmark {
@@ -60,14 +79,14 @@ func newBenchmark() requester.Benchmark {
 		Length:   3,
 		Duration: 4 * time.Second,
 		Records: []requester.Record{
-			{Time: time.Second},
-			{Time: time.Second},
-			{Time: time.Second},
+			{Time: 5 * time.Second},
+			{Time: 6 * time.Second},
+			{Time: 7 * time.Second},
 		},
 	}
 }
 
-func newConfig(tpl string) config.Global {
+func newConfigWithTemplate(tpl string) config.Global {
 	urlURL, _ := url.ParseRequestURI("https://a.b.com")
 	return config.Global{
 		Request: config.Request{URL: urlURL},
@@ -83,13 +102,77 @@ func checkSummary(t *testing.T, summary string) {
 Endpoint           https://a.b.com
 Requests           3/∞
 Errors             1
-Min response time  1000ms
-Max response time  1000ms
-Mean response time 1000ms
+Min response time  5000ms
+Max response time  7000ms
+Mean response time 6000ms
 Total duration     4000ms
 `[1:]
 
 	if summary != expSummary {
 		t.Errorf("\nexp summary:\n%q\ngot summary:\n%q", expSummary, summary)
 	}
+}
+
+func checkRequest(t *testing.T, r *http.Request, rep *output.Report) {
+	t.Helper()
+
+	if r == nil {
+		t.Fatal("returned nil request")
+	}
+
+	t.Run("set method to POST", func(t *testing.T) {
+		const expMethod = "POST"
+		if r.Method != expMethod {
+			t.Errorf("request method: exp %q, got %q", expMethod, r.Method)
+		}
+	})
+
+	t.Run("set target url to correct endpoint", func(t *testing.T) {
+		const expURL = "http://localhost:9998/v1/report"
+		if gotURL := fmt.Sprint(r.URL); gotURL != expURL {
+			t.Errorf("request method: exp %q, got %q", expURL, gotURL)
+		}
+	})
+
+	t.Run("set body to gob encoded report", func(t *testing.T) {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		gotReport := &output.Report{}
+		if err := gob.NewDecoder(bytes.NewReader(b)).Decode(gotReport); err != nil {
+			t.Fatal(err)
+		}
+		if !sameReports(rep, gotReport) {
+			t.Errorf("unexpected report:\nexp %#v\ngot %#v", rep, gotReport)
+		}
+	})
+}
+
+// sameReports performs a deep equality check for two output.Report,
+// ignoring Report.log value.
+func sameReports(a, b *output.Report) bool {
+	// cannot rely on reflect.DeepEqual for time.Time
+	// https://github.com/golang/go/issues/10089
+	if !a.Metadata.FinishedAt.Equal(b.Metadata.FinishedAt) {
+		return false
+	}
+
+	// times are equal, use exact same value for both reports
+	// so we can perform deep equality check
+	bMetadata := b.Metadata
+	bMetadata.FinishedAt = a.Metadata.FinishedAt
+
+	// exclude Report.log func
+	return reflect.DeepEqual(
+		output.Report{
+			Benchmark: a.Benchmark,
+			Metadata:  a.Metadata,
+		},
+		output.Report{
+			Benchmark: b.Benchmark,
+			Metadata:  bMetadata,
+		},
+	)
 }
