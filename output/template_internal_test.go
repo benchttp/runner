59aeebc4a13639dbb5ed4c7725651eb25a0cf6e5
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-03-09 00:25:05 +0100

    Write unit tests for Report.templateFuncs
    - "stats"
    - "event" - "return matching event"
    - "event" - "return 0 if no match"
    - "fail"

--- output/template_internal_test.go
+++ output/template_internal_test.go
@@ -3,6 +3,9 @@ package output
 import (
 	"errors"
 	"testing"
+	"time"
+
+	"github.com/benchttp/runner/requester"
 )
 
 func TestReport_applyTemplate(t *testing.T) {
@@ -49,5 +52,109 @@ func TestReport_applyTemplate(t *testing.T) {
 				t.Errorf("unexpected string: %q", gotStr)
 			}
 		})
+	}
+}
+
+func TestReport_templateFuncs(t *testing.T) {
+	t.Run("stats", func(t *testing.T) {
+		rep := newFilledReport()
+
+		v := retrieveTemplateFuncOrFatal(t, rep, "stats")
+
+		f, ok := v.(func() basicStats)
+		if !ok {
+			t.Fatalf("wrong type:\nexp func() basicStats\ngot %T", v)
+		}
+
+		if gotStats := f(); (gotStats != basicStats{
+			Min:  1 * time.Second,
+			Max:  3 * time.Second,
+			Mean: 2 * time.Second,
+		}) {
+			t.Errorf("unexpected stats: %+v", gotStats)
+		}
+	})
+
+	t.Run("event", func(t *testing.T) {
+		rep := newFilledReport()
+
+		v := retrieveTemplateFuncOrFatal(t, rep, "event")
+
+		f, ok := v.(func(requester.Record, string) time.Duration)
+		if !ok {
+			t.Fatalf("wrong type:\nexp func(requester.Record, string) time.Duration\ngot %T", v)
+		}
+
+		t.Run("return matching event", func(t *testing.T) {
+			rec := rep.Benchmark.Records[0]
+			if got, exp := f(rec, "event1"), rec.Events[1].Time; got != exp {
+				t.Errorf("unexpected time: exp %s, got %s", exp, got)
+			}
+		})
+
+		t.Run("return 0 if no match", func(t *testing.T) {
+			rec := rep.Benchmark.Records[0]
+			if got, exp := f(rec, "nomatch"), time.Duration(0); got != exp {
+				t.Errorf("unexpected time: exp %s, got %s", exp, got)
+			}
+		})
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		rep := newFilledReport()
+
+		v := retrieveTemplateFuncOrFatal(t, rep, "fail")
+
+		f, ok := v.(func(...interface{}) string)
+		if !ok {
+			t.Fatalf("wrong type:\nexp func(...interface{}) string\ngot %T", v)
+		}
+
+		if got := f("a", "b", "c"); got != "" {
+			t.Errorf("unexpected output: exp always %q, got %q", "", got)
+		}
+
+		gotErr := rep.errTplFailTriggered
+		if !errors.Is(gotErr, ErrTemplateFailTriggered) {
+			t.Fatalf("unexpected error:\nexp ErrTemplateFailTriggered\ngot %v", gotErr)
+		}
+		if gotMsg, expMsg := gotErr.Error(), "test failed: abc"; gotMsg != expMsg {
+			t.Errorf("unexpected error message:\nexp %q\ngot %q", expMsg, gotMsg)
+		}
+	})
+}
+
+// helpers
+
+func retrieveTemplateFuncOrFatal(t *testing.T, r *Report, name string) interface{} {
+	t.Helper()
+	v, exists := r.templateFuncs()[name]
+	if !exists {
+		t.Fatalf("template func %q does not exist", name)
+	}
+	return v
+}
+
+// newFilledReport returns a new report with some values set.
+func newFilledReport() *Report {
+	return &Report{
+		Benchmark: requester.Benchmark{
+			Records: []requester.Record{
+				{
+					Time: 1 * time.Second,
+					Events: []requester.Event{
+						{Name: "event0", Time: 400 * time.Millisecond},
+						{Name: "event1", Time: 600 * time.Millisecond},
+					},
+				},
+				{
+					Time: 3 * time.Second,
+					Events: []requester.Event{
+						{Name: "event0", Time: 2 * time.Second},
+						{Name: "event1", Time: 1 * time.Second},
+					},
+				},
+			},
+		},
 	}
 }
