7b5d91ce6f61d6f5923995ca312ddc9d420ec31b
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-03-09 10:31:39 +0100

    Implement output.ExportError.HasAuthError

--- output/error.go
+++ output/error.go
@@ -2,7 +2,10 @@ package output
 
 import (
 	"errors"
+	"net/http"
 	"strings"
+
+	"github.com/benchttp/runner/output/export"
 )
 
 var (
@@ -34,4 +37,20 @@ func (e *ExportError) Error() string {
 	}
 
 	return b.String()
+}
+
+// HasAuthError returns true if any of the errors in ExportError
+// is an export.HTTPResponseError with code http.StatusUnauthorized.
+func (e *ExportError) HasAuthError() bool {
+	for _, err := range e.Errors {
+		if !errors.Is(err, export.ErrHTTPResponse) {
+			continue
+		}
+		var errCode *export.HTTPResponseError
+		if !errors.As(err, &errCode) || errCode == nil {
+			continue
+		}
+		return errCode.Code == http.StatusUnauthorized
+	}
+	return false
 }
